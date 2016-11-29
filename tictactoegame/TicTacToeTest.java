bffd0da45d5222ac1c6207c3f720e4d34c4d9bf5
BagerB <bager.b@hotmail.de>
2016-11-29 16:43:34 +0100

    TicTacToe Hausaufgabe
    Die TicTacToe Hausaufgabe, ohne eine DrawFunktion und ohne den
    JUnit-Test. Würde es gut finden, wenn man den JUnit-Test nochmal
    zusammen in der Vorlesung besprechen könnte.

---
+++ tictactoegame/TicTacToeTest.java
@@ -0,0 +1,29 @@
+package tictactoegame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+	@Test
+	public void playerTurn() {
+			
+	}
+	
+	@Test
+	public void playerWon() {
+		
+	}
+	
+	@Test
+	public void computerWon() {
+		
+	}
+	
+	
+
+}
+
+
+//Ich habe das leider nicht hinbekommen. Würde es gut finden, wenn man das in der Vorlesung nochmal erklären könnte.
